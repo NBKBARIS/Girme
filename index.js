ad6ff05f0d068291ae3711dc83621554f4356b49
NBKBARIS <69206317+NBKBARIS@users.noreply.github.com>
2024-05-21 16:09:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
-const dotenv = require('dotenv'); 
-dotenv.config(); 
+require('dotenv').config(); // dotenv modülünü kullanarak .env dosyasındaki değişkenleri yükle
 
 const colors = require('colors');
 const title = require('./modules/title.js');
-const { Client } = require('discord.js');
+const { Client, Collection } = require('discord.js');
 const client = new Client({ intents: 32767 });
 title('Hosgeldiniz');
 
 require("./loader.js")(client);
 
 client.on('ready', () => {
-    console.log(`${client.user.tag} Olarak Giriş Yapıldı!`.green);
+    console.log(`${client.user.tag} Olarak Giris Yaptiniz!`.green);
     client.user.setPresence({ activities: [{ name: 'Cesur Bomber', type: 0 }], status: 'idle' });
 });
 
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.sendStatus(200);
-});
-
-app.listen(port, () => {
-  console.log(`Sunucu ${port} numaralı bağlantı noktasında çalışıyor.`);
-});
-
-// Heroku için gerekli değişiklikler
-if (process.env.DISCORD_TOKEN) {
-    client.login(process.env.DISCORD_TOKEN).catch(() => console.log('Tokeni Kontrol Ediniz'.red));
-} else {
-    console.log("DISCORD_TOKEN ortam değişkeni bulunamadı. Lütfen Heroku ayarlarınızı kontrol edin.");
-}
+client.login(process.env.DISCORD_TOKEN).catch(() => console.log('Tokeni Kontrol Ediniz'.red));
